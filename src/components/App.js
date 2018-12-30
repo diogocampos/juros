166744230fb227f94c05fcac2dcaaf5c53674282
Diogo Campos <diogocampos@users.noreply.github.com>
2018-12-30 16:10:14 -0200

    Add mode switching

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { actions } from '../state'
 
 class App extends Component {
   render() {
-    return <div className="App">Juros</div>
+    const { mode, switchToInstallmentMode, switchToInterestMode } = this.props
+    return (
+      <div className="App">
+        <nav>
+          <button onClick={switchToInterestMode}>Juros</button>
+          <button onClick={switchToInstallmentMode}>Parcela</button>
+        </nav>
+        <p>{mode}</p>
+      </div>
+    )
   }
 }
 
-export default App
+const mapStateToProps = state => ({
+  mode: state.mode,
+})
+
+export default connect(
+  mapStateToProps,
+  actions
+)(App)
