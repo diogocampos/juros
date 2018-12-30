dc44caf03288577674690b802b60cd21c093fe02
Diogo Campos <diogocampos@users.noreply.github.com>
2018-12-30 16:33:08 -0200

    Stub calculator components

---
+++ src/components/Calculator.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function InstallmentCalculator() {
+  return <p>[calculadora de parcela]</p>
+}
+
+export function InterestCalculator() {
+  return <p>[calculadora de juros]</p>
+}
