e8056cae91ce397e57a5ff62d53f449fe3337e63
Diogo Campos <diogocampos@users.noreply.github.com>
2018-12-30 21:43:55 -0200

    Add classes to `ModeSwitcher`

--- src/components/ModeSwitcher.js
+++ src/components/ModeSwitcher.js
@@ -1,19 +1,35 @@
+import classNames from 'classnames'
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { INSTALLMENT, INTEREST } from '../constants'
 import { actionCreators } from '../state'
 
 function ModeSwitcher(props) {
-  const { switchToInstallmentMode, switchToInterestMode } = props
+  const { mode, switchToInstallmentMode, switchToInterestMode } = props
   return (
-    <div>
-      <button onClick={switchToInterestMode}>Juros</button>
-      <button onClick={switchToInstallmentMode}>Parcela</button>
+    <div className="ModeSwitcher">
+      <button
+        className={classNames('mode', mode === INTEREST && 'active')}
+        onClick={switchToInterestMode}
+      >
+        Juros
+      </button>
+      <button
+        className={classNames('mode', mode === INSTALLMENT && 'active')}
+        onClick={switchToInstallmentMode}
+      >
+        Parcela
+      </button>
     </div>
   )
 }
 
+const mapStateToProps = state => ({
+  mode: state.mode,
+})
+
 export default connect(
-  null,
+  mapStateToProps,
   actionCreators
 )(ModeSwitcher)
