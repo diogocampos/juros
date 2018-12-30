c6d523cd07520a88c93981e08602738270541f99
Diogo Campos <diogocampos@users.noreply.github.com>
2018-12-30 16:34:34 -0200

    Switch calculator component when the mode changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { InstallmentCalculator, InterestCalculator } from './Calculator'
+import { INSTALLMENT_MODE, INTEREST_MODE } from '../constants'
 import { actions } from '../state'
+
+const calculators = {
+  [INSTALLMENT_MODE]: InstallmentCalculator,
+  [INTEREST_MODE]: InterestCalculator,
+}
 
 function App(props) {
   const { mode, switchToInstallmentMode, switchToInterestMode } = props
+  const Calculator = calculators[mode]
+
   return (
     <div className="App">
       <nav>
         <button onClick={switchToInterestMode}>Juros</button>
         <button onClick={switchToInstallmentMode}>Parcela</button>
       </nav>
-      <p>{mode}</p>
+      <Calculator />
     </div>
   )
 }
