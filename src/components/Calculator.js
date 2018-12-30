a2a048d186db1a524483117d2571fbd6cfe5d80a
Diogo Campos <diogocampos@users.noreply.github.com>
2018-12-30 20:11:24 -0200

    Render calculator fields and buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import NumberField from './NumberField'
+import { DEPOSIT, INSTALLMENT, INTEREST, LENGTH, PRICE } from '../constants'
+
+// NumberField props
+const deposit = { name: DEPOSIT, label: 'Entrada' }
+const installment = { name: INSTALLMENT, label: 'Valor da parcela' }
+const interest = { name: INTEREST, label: 'Taxa de juros' }
+const length = { name: LENGTH, label: 'Número de parcelas' }
+const price = { name: PRICE, label: 'Valor à vista' }
+
+const fieldsByMode = {
+  [INSTALLMENT]: [price, deposit, length, interest],
+  [INTEREST]: [price, deposit, length, installment],
+}
 
 function Calculator(props) {
   const { mode } = props
-  return <p>[{mode} calculator]</p>
+  const fields = fieldsByMode[mode]
+  return (
+    <form>
+      {fields.map(fieldProps => (
+        <NumberField key={fieldProps.name} {...fieldProps} />
+      ))}
+      <button type="button">Limpar</button>
+      <button type="button">Calcular</button>
+    </form>
+  )
 }
 
 const mapStateToProps = state => ({
