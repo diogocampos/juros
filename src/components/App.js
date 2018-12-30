9e4b04bd65373220060a058501da77364b0e5eac
Diogo Campos <diogocampos@users.noreply.github.com>
2018-12-30 16:11:54 -0200

    Use functions instead of classes

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
 import { actions } from '../state'
 
-class App extends Component {
-  render() {
-    const { mode, switchToInstallmentMode, switchToInterestMode } = this.props
-    return (
-      <div className="App">
-        <nav>
-          <button onClick={switchToInterestMode}>Juros</button>
-          <button onClick={switchToInstallmentMode}>Parcela</button>
-        </nav>
-        <p>{mode}</p>
-      </div>
-    )
-  }
+function App(props) {
+  const { mode, switchToInstallmentMode, switchToInterestMode } = props
+  return (
+    <div className="App">
+      <nav>
+        <button onClick={switchToInterestMode}>Juros</button>
+        <button onClick={switchToInstallmentMode}>Parcela</button>
+      </nav>
+      <p>{mode}</p>
+    </div>
+  )
 }
 
 const mapStateToProps = state => ({
