5dbb3d9f97b41cdc43be72ce03f43eeab484e822
Diogo Campos <diogocampos@users.noreply.github.com>
2018-12-31 14:50:34 -0200

    Convert `NumberField` into a class

--- src/components/NumberField.js
+++ src/components/NumberField.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
-function NumberField(props) {
-  const { label, name, value, onChange } = props
-  return (
-    <label className="NumberField">
-      {label}
-      <input
-        className="NumberField-input"
-        type="text"
-        pattern="[0-9]*"
-        name={name}
-        value={value}
-        onChange={onChange}
-      />
-    </label>
-  )
+class NumberField extends React.Component {
+  render() {
+    const { label, name, value, onChange } = this.props
+    return (
+      <label className="NumberField">
+        {label}
+        <input
+          className="NumberField-input"
+          type="text"
+          pattern="[0-9]*"
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+      </label>
+    )
+  }
 }
 
 export default NumberField
