94f0a18008035334dfc01a9515480150717960d8
Diogo Campos <diogocampos@users.noreply.github.com>
2018-12-31 12:31:50 -0200

    Attach state to `Calculator` and `NumberField`

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 
 import NumberField from './NumberField'
 import * as k from '../constants'
+import { actionCreators } from '../state'
 
 const fieldNamesByMode = {
   [k.INSTALLMENT]: [k.PRICE, k.DEPOSIT, k.LENGTH, k.INTEREST],
@@ -19,18 +20,37 @@ const fieldProps = {
 }
 
 class Calculator extends React.Component {
+  handleChange = event => {
+    const { name, value } = event.target
+    this.props.setValue(name, value)
+  }
+
   render() {
-    const { mode } = this.props
+    const { mode, values, clearValues, calculateResults } = this.props
     const fieldNames = fieldNamesByMode[mode]
     return (
       <form className="Calculator">
         {fieldNames.map(name => (
-          <NumberField key={name} name={name} {...fieldProps[name]} />
+          <NumberField
+            key={name}
+            name={name}
+            value={values[name]}
+            onChange={this.handleChange}
+            {...fieldProps[name]}
+          />
         ))}
-        <button className="Calculator-button" type="button">
+        <button
+          className="Calculator-button"
+          type="button"
+          onClick={clearValues}
+        >
           Limpar
         </button>
-        <button className="Calculator-button primary" type="button">
+        <button
+          className="Calculator-button primary"
+          type="button"
+          onClick={calculateResults}
+        >
           Calcular
         </button>
       </form>
@@ -40,6 +60,10 @@ class Calculator extends React.Component {
 
 const mapStateToProps = state => ({
   mode: state.mode,
+  values: state.values,
 })
 
-export default connect(mapStateToProps)(Calculator)
+export default connect(
+  mapStateToProps,
+  actionCreators
+)(Calculator)
--- src/components/NumberField.js
+++ src/components/NumberField.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 function NumberField(props) {
-  const { label } = props
+  const { label, name, value, onChange } = props
   return (
     <label className="NumberField">
       {label}
-      <input className="NumberField-input" type="text" pattern="[0-9]*" />
+      <input
+        className="NumberField-input"
+        type="text"
+        pattern="[0-9]*"
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
     </label>
   )
 }
