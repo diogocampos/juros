ae79ea69beb8938c6daf4ddae33b48aaf0ebd77b
Diogo Campos <diogocampos@users.noreply.github.com>
2019-01-01 13:43:33 -0200

    Fix failing test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
+import { wrap } from '../state'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(wrap(<App />), div)
   ReactDOM.unmountComponentAtNode(div)
 })
