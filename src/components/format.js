89b96683fbeba2f27fe1e3159100d9c5f2091aa8
Diogo Campos <diogocampos@users.noreply.github.com>
2019-01-01 21:27:07 -0200

    Add doc comments to format.js

--- src/components/format.js
+++ src/components/format.js
@@ -1,6 +1,12 @@
 const DECIMAL_SEPARATOR = ','
 const THOUSANDS_SEPARATOR = '.'
 
+/**
+ * Formats a number as a string with thousands and decimal separators.
+ * @param {number} number The number
+ * @param {number} decimals The amount of decimal digits to include
+ * @returns {string} The resulting string
+ */
 export function formatNumber(number, decimals = 2) {
   const [integer, fraction] = number.toFixed(decimals).split('.')
   const modulo = integer.length % 3
@@ -14,6 +20,12 @@ export function formatNumber(number, decimals = 2) {
   )
 }
 
+/**
+ * Parses a string of digits into a number.
+ * @param {string} textValue The string
+ * @param {number} decimals The amount of decimal digits to consider
+ * @returns {number} The corresponding number
+ */
 export function parseDigits(textValue, decimals = 2) {
   let digits = textValue.replace(/[^\d]/g, '')
   if (digits.length < decimals) digits = padStart(digits, decimals, '0')
@@ -22,7 +34,7 @@ export function parseDigits(textValue, decimals = 2) {
   return +(digits.slice(0, cut) + '.' + digits.slice(cut))
 }
 
-function padStart(string, targetLength, padString) {
+function padStart(string, targetLength, padString = ' ') {
   for (var padding = ''; padding.length + string.length < targetLength; ) {
     padding += padString
   }
