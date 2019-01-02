fc4a4664fa4a731b1cb67607a1511efee5820bf9
Diogo Campos <diogocampos@users.noreply.github.com>
2019-01-02 10:30:58 -0200

    Updates

---
+++ precache-manifest.2f01a245200c2643b6422ea4edbc1a3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35aca8b988565434bf6b",
+    "url": "./static/css/main.c7e54fca.chunk.css"
+  },
+  {
+    "revision": "35aca8b988565434bf6b",
+    "url": "./static/js/main.35aca8b9.chunk.js"
+  },
+  {
+    "revision": "381406f6fb0e8374ad13",
+    "url": "./static/js/1.381406f6.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4da0ba61d8b744f99fe4f4b3210504cc",
+    "url": "./index.html"
+  }
+];
